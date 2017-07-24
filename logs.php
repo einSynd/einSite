cc8ba9b606b76a7ff7e494d18eeef6e989594a9a
einSynd <knivesu@gmail.com>
2017-07-24 02:26:10 -0700

    Updated Log Viewers
    Changed date label to a text box, can manually enter dates now. Updates
    to say it's trying to load a log. Fixed incorrect colors when colors are
    disabled on any background other than black.

--- logs.php
+++ logs.php
@@ -2,7 +2,9 @@
 <head>
 <title>IRC Log Viewer</title>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js" type="text/javascript"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="autolinker.min.js"></script>
+<meta name="viewport" content="width=device-width, intial-scale=1" />
 <style>
 .fore00 { color: #FFFFFF; }
 .fore01 { color: #000000; }
@@ -38,142 +40,193 @@
 .back14 { background-color:#7F7F7F; }
 .back15 { background-color:#D2D2D2; }
 
-body, .nocolor{ color: white; background-color: black; }
+body, .nocolor-black{ color: white; background-color: black; }
+.nocolor-white{ color: black; background-color: white; }
+.nocolor-gray { color: black; background-color: gray;  }
 .spoiler.nocolor{ color: black; background-color: black; }
 .hidden { font-size: 0px; }
 a{ color: inherit !important; }
 
 html, body { margin: 0px; padding: 0px; }
 #headBar { position: fixed; width: 100%; border-bottom: 1px solid #000; background: inherit; 
-			top: 0px; padding: 2 5; }
+            top: 0px; padding: 2 5; }
 #colorButtons { float: right; padding: 0 5 0 0; }
 input { margin: 0px; padding: 0px; }
 #content { display: inline-block; padding: 10 0 5 5; margin: 15 0 0 0;}
+#currentLog { width: 80px; }
+.line { display: inline; }
+
 </style>
 </head>
 <body>
 <script type="text/javascript">
+var currentColor = "black";
+
 function setColor(match, p1, offset, string){
-	if(p1.length == 1){ p1 = "0" + p1; }
-	return "<span id='color' class='fore" + p1 + " backNo'>";
+    if(p1.length == 1){ p1 = "0" + p1; }
+    return "<span id='color' class='fore" + p1 + " backNo'>";
 }
 
 function setColor2(match, p1, p2, offset, string){
-	if(p1.length == 1){ p1 = "0" + p1; }
-	if(p2.length == 1){ p2 = "0" + p2; }
-	
-	var spoiler = "";
-	if(p1 == p2){ spoiler = " spoiler"; }
+    if(p1.length == 1){ p1 = "0" + p1; }
+    if(p2.length == 1){ p2 = "0" + p2; }
+    
+    var spoiler = "";
+    if(p1 == p2){ spoiler = " spoiler"; }
 
-	return "<span id='color' class='fore" + p1 + " back" + p2 + spoiler + "'>";
+    return "<span id='color' class='fore" + p1 + " back" + p2 + spoiler + "'>";
 }
 
 function addAnchor(match, p1, offset, string){
-	return "<span id='"+p1+"'>"+p1+"</span>";
+    return "<span id='"+p1+"'>"+p1+"</span>";
 }
 
 function changeColor(bgColor){
-	$("body").css("background",bgColor);
-	
-	color="black";
-	if(bgColor=="black"){
-		color = "white"; }
+    currentColor = bgColor;
+    $("body").css("background",bgColor);
+    
+    color="black";
+    if(bgColor=="black"){
+        color = "white"; }
 
-	$("body").css("color",color);
-	$("#headBar").css("border-color",color);
+    $("body").css("color",color);
+    $("#headBar").css("border-color",color);
 }
 
 function fileCheck(date, first){
-	if(date==""){ date="today"; }
-	//Get today minus five hours because GMT-5
-	var today = new Date();
-	today.setTime(today.getTime() - 5 * 60 * 60 * 1000);
-	
-	//Strip off the actual time, just need the day
-	today = today.toISOString().split("T")[0];
-	
-	if(date == "today") {
-		date = today;
-	}
-	
-    $("#currentLog").html(date);
-	file = "http://" + window.location.host + "/vgzlogs/%23vidyagamez."+date+".log";
-	$.get(file)
+    if(date==""){ date="today"; }
+    //Get today minus five hours because GMT-5
+    var today = new Date();
+    today.setTime(today.getTime() - 5 * 60 * 60 * 1000);
+    
+    //Strip off the actual time, just need the day
+    today = today.toISOString().split("T")[0];
+    
+    if(date == "today") {
+        date = today;
+    }
+    
+    $("#currentLog").val(date);
+    $("#content").html("Fetching log, please wait.");
+    file = "http://" + window.location.host + "/vgzlogs/%23vidyagamez."+date+".log";
+    $.get(file)
     .done(function(data) { 
-		//Parse the raw log into something vaguely HTML compatible
-		//Split and rejoin the greater than and less than signs; a bit faster than global regex
-		data = data.split("<").join("&lt;");
-		data = data.split(">").join("&gt;");
+        //Parse the raw log into something vaguely HTML compatible
+        //Split and rejoin the greater than and less than signs; a bit faster than global regex
+        data = data.split("<").join("&lt;");
+        data = data.split(">").join("&gt;");
         data = "<span class='line'>" + data;        
-		//Turn unicode 0002 (stx) into Strong tag for bold
-		data = data.replace(/\u0002/g,"<strong>");
-		//Turn unicode 0003 (etx) plus the following numbers into spans for color tags
-		data = data.replace(/\u0003(\d{1,2}),(\d{1,2})/g,setColor2);
-		data = data.replace(/\u0003(\d{1,2})/g, setColor);
-		//Turn unicode 0003 (etx) without a following numbers into end span
-		data = data.replace(/\u0003/g, "");
-		//End the line
-		data = data.replace(/\r\n/g,"</strong></span></span><br />\r\n<span class='line'>");
+        //Turn unicode 0002 (stx) into Strong tag for bold
+        data = data.replace(/\u0002/g,"<strong>");
+        //Turn unicode 0003 (etx) plus the following numbers into spans for color tags
+        data = data.replace(/\u0003(\d{1,2}),(\d{1,2})/g,setColor2);
+        data = data.replace(/\u0003(\d{1,2})/g, setColor);
+        //Turn unicode 0003 (etx) without a following numbers into end span
+        data = data.replace(/\u0003/g, "");
+        //End the line
+        data = data.replace(/\r\n/g,"</strong></span></span><br />\r\n<span class='line'>");
         //Add anchors (.php#stuff) for the timestamps
         data = data.replace(/(\d{4}-\d{2}-\d{2}) /g,"");
-		data = data.replace(/(\d{2}:\d{2}:\d{2})/g, addAnchor);
+        data = data.replace(/(\d{2}:\d{2}:\d{2})/g, addAnchor);
         //Give the JOIN and QUIT messages a span so they can be toggled
         data = data.replace(/JOIN/g,"<span class='connect'>JOIN</span>");
         data = data.replace(/QUIT/g,"<span class='connect'>QUIT</span>");
         var anchored = Autolinker.link(data);
-		$("#content").html(anchored);
-		
-		if(date == today){
-			$("#dateUp").prop("disabled",true);
-		} else {
-			$("#dateUp").prop("disabled",false);
-		}
-		
-		if(date == "2015-01-01"){
-			$("#dateDown").prop("disabled",true);
-		} else {
-			$("#dateDown").prop("disabled",false);
-		}
-		
-		if(typeof first != 'undefined' && window.location.hash != ""){
-			location.hash = window.location.hash;
-			window.scrollTo(0, window.scrollY - 28);
-		}
-		
+        $("#content").html(anchored);
+        
+        if(date == today){
+            $("#dateUp").prop("disabled",true);
+        } else {
+            $("#dateUp").prop("disabled",false);
+        }
+        
+        if(date == "2015-01-01"){
+            $("#dateDown").prop("disabled",true);
+        } else {
+            $("#dateDown").prop("disabled",false);
+        }
+        
+        if(typeof first != 'undefined' && window.location.hash != ""){
+            location.hash = window.location.hash;
+            window.scrollTo(0, window.scrollY - 28);
+        }
+        
     }).fail(function() { 
         $("#content").html("There is no log for this date.");
     })
 }
 
 function changeDate(direction){
-	var current = $("#currentLog").html();
-	var oneDay = 86400000;
-	
-	var newDay = Date.parse(current);
-	if( direction == "up" ) { newDay = newDay + oneDay; }
-	if( direction == "down" ) { newDay = newDay - oneDay; }
-	newDay = new Date(newDay);
-	newDay = newDay.toISOString().split("T")[0];
-	
-	$("#content").html("");
-	fileCheck(newDay);
+    var current = $("#currentLog").html();
+    var oneDay = 86400000;
+    
+    var newDay = Date.parse(current);
+    if( direction == "up" ) { newDay = newDay + oneDay; }
+    if( direction == "down" ) { newDay = newDay - oneDay; }
+    newDay = new Date(newDay);
+    newDay = newDay.toISOString().split("T")[0];
+    
+    $("#content").html("");
+    fileCheck(newDay);
+}
+
+function goDate(){
+    var toGo = $("#currentLog").val();
+    
+    if( toGo.length > 10 || toGo.length < 8){
+        alert("Invalid date format, must be YYYY-MM-DD");
+        return
+    }
+    
+    var dateReg = /^(\d{4}).(\d{1,2}).(\d{1,2})$/;
+    
+    toGo = dateReg.exec(toGo);
+    
+    toYear = toGo[1];
+    toMonth = toGo[2];
+    toDay = toGo[3];
+    
+    if( toMonth < 1 || toMonth > 12 ){
+        alert("Invalid date format, must be YYYY-MM-DD");
+        return
+    }
+    
+    if( toDay < 1 || toDay > 31 ){
+        alert("Invalid date format, must be YYYY-MM-DD");
+        return
+    }
+    
+    var newDay = Date.parse(toYear + "-" + toMonth + "-" + toDay);
+    
+    newDay = new Date(newDay);
+    newDay = newDay.toISOString().split("T")[0];
+    fileCheck(newDay);
 }
 
 $(document).ready(function(){
-	fileCheck("<?php if(isset($_GET["d"])) { echo $_GET["d"]; } ?>", true);
-	
+    var btnGoDate = document.getElementById("goDate");
+    var txtCurLog = document.getElementById("currentLog");
+    
+    txtCurLog.addEventListener("keypress", function(event) {
+        if (event.keyCode == 13)
+            btnGoDate.click();
+    });
+    
+    fileCheck("<?php if(isset($_GET["d"])) { echo $_GET["d"]; } ?>", true);
+    $("#goDate").prop("disabled",false);
 });
+
 </script>
-<div id="headBar">
-<input type="button" onclick="$('span').toggleClass('nocolor');" value="Toggle Colors" /> 
+<nav id="headBar">
+<input type="button" onclick="$('span').toggleClass('nocolor-'+currentColor);" value="Toggle Colors" /> 
 <input type="button" onclick="$('.connect').parent().toggleClass('hidden');" value="Toggle Connections" />&nbsp; &nbsp; &nbsp; &nbsp;
-<input type="button" onclick="changeDate('down');" value="<" /><span id="currentLog"></span><input type="button" id="dateUp" onclick="changeDate('up');" value=">" />&nbsp; &nbsp; &nbsp; &nbsp;
+<input type="button" onclick="changeDate('down');" value="<" /><input type=text id="currentLog" /><input type="button" id="goDate" onclick="goDate();" value="Go" /><input type="button" id="dateUp" onclick="changeDate('up');" value=">" />&nbsp; &nbsp; &nbsp; &nbsp;
 Scroll: <input type="button" onclick="$('body').scrollTop(0);" value="&nbsp;&#8593;&nbsp;" />
 <input type="button" onclick="$('body').scrollTop($('body').height());" value="&nbsp;&#8595;&nbsp;" />
 <div id="colorButtons">Background: 
 <input type="button" onClick="changeColor('white')" value="White" /><input type="button" onClick="changeColor('gray')" value="Gray" /><input type="button" onClick="changeColor('black')" value="Black" />
 </div>
-</div>
+</nav>
 <div id="content"></div>
 </body>
 </html>
